08a9ca1a985fc8913aeebd4c211365ca4abac75e
Vinicius <vcruvinelr@gmail.com>
2020-05-25 15:05:05 -0300

    versao com geocode

--- auroraTerritory.py
+++ auroraTerritory.py
@@ -11,19 +11,36 @@ locale.setlocale(locale.LC_TIME, "ptb")
 if __name__ == '__main__':
     
     gp = arcgisscripting.create()
-    camadaEntrada = arcpy.GetParameterAsText(0)
-    displayField = arcpy.GetParameterAsText(1)
-    pastaProjeto = arcpy.GetParameterAsText(2)
-    territoryName = arcpy.GetParameterAsText(3)
-    pointsClientes = arcpy.GetParameterAsText(4)
-    minimoCapacity = arcpy.GetParameterAsText(5)
-    maximoCapacity = arcpy.GetParameterAsText(6)
-    quantidadeAreas = arcpy.GetParameterAsText(7)
+    tabelaGeocode = arcpy.GetParameterAsText(0)
+    addressLocator = arcpy.GetParameterAsText(1)
+    saidafeaturePontos = arcpy.GetParameterAsText(2)
+    camadaEntrada = arcpy.GetParameterAsText(3)
+    displayField = arcpy.GetParameterAsText(4)
+    pastaProjeto = arcpy.GetParameterAsText(5)
+    territoryName = arcpy.GetParameterAsText(6)
+    minimoCapacity = arcpy.GetParameterAsText(7)
+    maximoCapacity = arcpy.GetParameterAsText(8)
+    metodoTerritorios = arcpy.GetParameterAsText(9)
+    quantidadeAreas = arcpy.GetParameterAsText(10)
+    goodBalance = arcpy.GetParameterAsText(11)
     layerOutput = "{0}\\{1}\\tdlayer.lyr".format(pastaProjeto, territoryName)
 
     try:
         
         arcpy.AddMessage("Iniciando Processo")
+        arcpy.AddMessage("Convertendo Tabela de Enderecos")
+
+        arcpy.GeocodeAddresses_geocoding(
+            tabelaGeocode, 
+            addressLocator, 
+            "Address 'ENDERECO CLIE' VISIBLE NONE;Neighborhood 'BAIRRO CLIE' VISIBLE NONE;City 'CIDADE CLIE' VISIBLE NONE;Region 'ESTADO CLIE' VISIBLE NONE;Postal <None> VISIBLE NONE", 
+            saidafeaturePontos, 
+            "STATIC", 
+            "", 
+            ""
+            )
+
+        arcpy.AddMessage("Enderecos convertidos com sucesso. Iniciando processo - Criar Solucao de Territorio")
 
         gp.CreateTerritorySolution_TD(
             camadaEntrada,
@@ -38,7 +55,7 @@ if __name__ == '__main__':
 
         gp.ImportVariablesBySpatialJoin_TD(
             layerOutput, 
-            pointsClientes, 
+            saidafeaturePontos, 
             "Count"
             )
         
@@ -47,13 +64,13 @@ if __name__ == '__main__':
         gp.SetupLevelCapacityConstraints_TD(
             layerOutput, 
             "Territories[1]", 
-            "{0}.Count_SUM {1} # {2} 100".format(pointsClientes.split("\\")[-1], minimoCapacity, maximoCapacity)
+            "{0}.Count_SUM {1} # {2} 100".format(saidafeaturePontos.split("\\")[-1], minimoCapacity, maximoCapacity)
         )
 
         gp.SetupLevelBalancingVariables_TD(
             layerOutput, 
             "Territories[1]", 
-            "{0}.Count_SUM 100".format(pointsClientes.split("\\")[-1])
+            "{0}.Count_SUM 100".format(saidafeaturePontos.split("\\")[-1])
         )
     
         gp.SetupLevelTerritoryShapeParameters_TD(
@@ -64,7 +81,7 @@ if __name__ == '__main__':
             "", 
             "", 
             "", 
-            "30", 
+            "{0}".format(goodBalance), 
             "USE_ALL_FEATURES", 
             "GEOMETRIC_CENTER", 
             ""
@@ -79,22 +96,43 @@ if __name__ == '__main__':
             ""
         )
 
-        gp.CreateTerritories_TD(
-            layerOutput, 
-            "Territories[1]", 
-            "REMOVE_TERRITORIES", 
-            "USER_DEFINED", 
-            quantidadeAreas, 
-            "CREATE_FROM_OPTIMAL_LOCATIONS", 
-            "", 
-            "", 
-            "", 
-            "", 
-            "", 
-            "ALL_NAMES", 
-            "DO_NOT_AUTO_FILL_EXTENT", 
-            "DO_NOT_OVERRIDE_CONSTRAINTS"
-            )
+        if metodoTerritorios == "Automatico":
+
+            gp.CreateTerritories_TD(
+                layerOutput, 
+                "Territories[1]", 
+                "REMOVE_TERRITORIES", 
+                "OPTIMAL_NUMBER", 
+                "", 
+                "CREATE_FROM_OPTIMAL_LOCATIONS", 
+                "", 
+                "", 
+                "", 
+                "", 
+                "", 
+                "ALL_NAMES", 
+                "DO_NOT_AUTO_FILL_EXTENT", 
+                "DO_NOT_OVERRIDE_CONSTRAINTS"
+                )
+
+        else:
+
+            gp.CreateTerritories_TD(
+                layerOutput, 
+                "Territories[1]", 
+                "REMOVE_TERRITORIES", 
+                "USER_DEFINED", 
+                quantidadeAreas, 
+                "CREATE_FROM_OPTIMAL_LOCATIONS", 
+                "", 
+                "", 
+                "", 
+                "", 
+                "", 
+                "ALL_NAMES", 
+                "DO_NOT_AUTO_FILL_EXTENT", 
+                "DO_NOT_OVERRIDE_CONSTRAINTS"
+                ) 
         
         arcpy.AddMessage("Processo finalizado com sucesso!")
 
